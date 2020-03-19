6d0f8addcadf8f171be0ff5ad1fb1caa19202b9b
Charlotte Curran <charlottemariecurran@gmail.com>
2020-03-19 10:07:10 +0100

    set up theme and global styles

---
+++ frontend/src/Theme/GlobalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+body {
+  font-family: "rooney-sans", Avenir-Book, Calibri, sans-serif;
+  color: #424242;
+  line-height: 1.4;
+  font-size: 12px;
+}
+`;
---
+++ frontend/src/Theme/Theme.js
@@ -0,0 +1,16 @@
+export const Theme = {
+  color: {
+    blue: "#20A1D4",
+    green: "#1CBC2C",
+    orange: "#EF5F3C",
+    black: "#424242",
+    grey: "#777",
+    lightGrey: "#eaeaea",
+    white: "#FFF"
+  },
+  fonts: {
+    rooneyWeb: "rooney-web",
+    rooneySans: "rooney-sans",
+    AT: "AmericanTypewriter"
+  }
+};
